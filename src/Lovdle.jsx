1944a01e1ca34177e553411bd90f33858cba7465
Alvaro <alvarofernandezdelalamo@gmail.com>
2026-02-04 01:25:56 +0100

    Changed styles to start principal functionality on react

--- src/Lovdle.jsx
+++ src/Lovdle.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './Lovdle.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Lovdle() {
+  const [board, setBoard] = useState(Array(6).fill(""));
+  const [currentRow, setCurrentRow] = useState(0);
+  const solution = "AMOR";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div className="font-fredoka min-h-screen bg-fuchsia-50 text-gray-900 flex flex-col items-center justify-center">
+      <h1 className="text-9xl font-semibold mb-8">LOVDLE</h1>
+
+      {/* Tablero */}
+      <div className="grid grid-rows-6 gap-2">
+        {board.map((row, i) => (
+          <Row key={i} word={row} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/Lovdle.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function Row({ word }) {
+  // Suponiendo palabras de 5 letras
+  const cells = Array(5).fill(""); 
+  
+  return (
+    <div className="grid grid-cols-5 gap-2 mb-5">
+      {cells.map((_, i) => (
+        <div key={i} className="w-19 h-19 border-2 border-slate-700 flex items-center justify-center text-2xl font-bold uppercase">
+          {word[i] || ""}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Lovdle;
